9a5d1b9482520d3eeccf97191257c5800743d023
Mark Fisher <mark.j.fisher@gmail.com>
2025-03-18 21:14:14 +0000

    building up the post tests

--- fujinet-hal/tests/adapters/ffi/network_test.rs
+++ fujinet-hal/tests/adapters/ffi/network_test.rs
@@ -1,60 +1,61 @@
 use std::ffi::CString;
-use fujinet_hal::adapters::ffi::network::{network_init, network_open};
+use fujinet_hal::adapters::ffi::network::{network_init, network_open, network_http_post};
+use fujinet_hal::adapters::ffi::error::{FN_ERR_BAD_CMD, FN_ERR_OK};
 
 #[test]
 fn test_network_init() {
-    assert_eq!(network_init(), 0); // FN_ERR_OK
+    assert_eq!(network_init(), FN_ERR_OK);
 }
 
 #[test]
 fn test_network_open_null_devicespec() {
-    assert_eq!(network_open(std::ptr::null(), 0, 0), 2); // FN_ERR_BAD_CMD
+    assert_eq!(network_open(std::ptr::null(), 0, 0), FN_ERR_BAD_CMD);
 }
 
 #[test]
 fn test_network_open_invalid_utf8() {
     let invalid = unsafe { CString::from_vec_unchecked([0xFF, 0xFE, 0x00].to_vec()) };
-    assert_eq!(network_open(invalid.as_ptr(), 0, 0), 2); // FN_ERR_BAD_CMD
+    assert_eq!(network_open(invalid.as_ptr(), 0, 0), FN_ERR_BAD_CMD);
 }
 
 #[test]
 fn test_network_open_invalid_url() {
     let invalid = CString::new("not_a_url").unwrap();
-    assert_eq!(network_open(invalid.as_ptr(), 0, 0), 2); // FN_ERR_BAD_CMD
+    assert_eq!(network_open(invalid.as_ptr(), 0, 0), FN_ERR_BAD_CMD);
 }
 
 #[test]
 fn test_network_open_valid_urls() {
     // Test N1: (default unit)
     let url1 = CString::new("N:http://example.com").unwrap();
-    assert_eq!(network_open(url1.as_ptr(), 4, 0), 0); // FN_ERR_OK
+    assert_eq!(network_open(url1.as_ptr(), 4, 0), FN_ERR_OK);
 
     // Test N2:
     let url2 = CString::new("N2:http://example.com").unwrap();
-    assert_eq!(network_open(url2.as_ptr(), 8, 0), 0); // FN_ERR_OK
+    assert_eq!(network_open(url2.as_ptr(), 8, 0), FN_ERR_OK);
 
     // Test N8:
     let url8 = CString::new("N8:http://example.com").unwrap();
-    assert_eq!(network_open(url8.as_ptr(), 12, 0), 0); // FN_ERR_OK
+    assert_eq!(network_open(url8.as_ptr(), 12, 0), FN_ERR_OK);
 
     // Test N9: (invalid unit)
     let url9 = CString::new("N9:http://example.com").unwrap();
-    assert_eq!(network_open(url9.as_ptr(), 4, 0), 2); // FN_ERR_BAD_CMD
+    assert_eq!(network_open(url9.as_ptr(), 4, 0), FN_ERR_BAD_CMD);
 }
 
 #[test]
 fn test_network_open_different_protocols() {
     // Test HTTP
     let http = CString::new("N1:http://example.com").unwrap();
-    assert_eq!(network_open(http.as_ptr(), 4, 0), 0); // FN_ERR_OK
+    assert_eq!(network_open(http.as_ptr(), 4, 0), FN_ERR_OK);
 
     // Test HTTPS
     let https = CString::new("N2:https://example.com").unwrap();
-    assert_eq!(network_open(https.as_ptr(), 4, 0), 0); // FN_ERR_OK
+    assert_eq!(network_open(https.as_ptr(), 4, 0), FN_ERR_OK);
 
     // Test invalid protocol
     let invalid = CString::new("N3:ftp://example.com").unwrap();
-    assert_eq!(network_open(invalid.as_ptr(), 4, 0), 2); // FN_ERR_BAD_CMD
+    assert_eq!(network_open(invalid.as_ptr(), 4, 0), FN_ERR_BAD_CMD);
 }
 
 #[test]
@@ -68,6 +69,44 @@ fn test_network_open_multiple_devices() {
 
     for (url, mode) in urls.iter() {
         let c_url = CString::new(*url).unwrap();
-        assert_eq!(network_open(c_url.as_ptr(), *mode, 0), 0); // FN_ERR_OK
+        assert_eq!(network_open(c_url.as_ptr(), *mode, 0), FN_ERR_OK);
     }
-} 
+}
+
+#[test]
+fn test_network_http_post_null_pointers() {
+    // Test null devicespec
+    let data = CString::new("test data").unwrap();
+    assert_eq!(network_http_post(std::ptr::null(), data.as_ptr()), FN_ERR_BAD_CMD);
+
+    // Test null data
+    let devicespec = CString::new("N1:http://example.com").unwrap();
+    assert_eq!(network_http_post(devicespec.as_ptr(), std::ptr::null()), FN_ERR_BAD_CMD);
+
+    // Test both null
+    assert_eq!(network_http_post(std::ptr::null(), std::ptr::null()), FN_ERR_BAD_CMD);
+}
+
+#[test]
+fn test_network_http_post_invalid_utf8() {
+    let devicespec = CString::new("N1:http://example.com").unwrap();
+    let invalid = unsafe { CString::from_vec_unchecked([0xFF, 0xFE, 0x00].to_vec()) };
+    
+    // Test invalid UTF-8 in data
+    assert_eq!(network_http_post(devicespec.as_ptr(), invalid.as_ptr()), FN_ERR_BAD_CMD);
+
+    // Test invalid UTF-8 in devicespec
+    let data = CString::new("test data").unwrap();
+    assert_eq!(network_http_post(invalid.as_ptr(), data.as_ptr()), FN_ERR_BAD_CMD);
+}
+
+#[test]
+fn test_network_http_post_success() {
+    // First open a device
+    let devicespec = CString::new("N1:http://example.com").unwrap();
+    assert_eq!(network_open(devicespec.as_ptr(), 4, 0), FN_ERR_OK);
+
+    // Then post to it
+    let data = CString::new("test data").unwrap();
+    assert_eq!(network_http_post(devicespec.as_ptr(), data.as_ptr()), FN_ERR_OK);
+}
