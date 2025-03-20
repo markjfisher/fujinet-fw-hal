1a76048cdf2e6e5ebb038037d764e102031b7cd9
Mark Fisher <mark.j.fisher@gmail.com>
2025-03-20 23:16:46 +0000

    trying to fix

--- fujinet-hal/src/platform/x86/network/manager.rs
+++ fujinet-hal/src/platform/x86/network/manager.rs
@@ -1,11 +1,10 @@
 use std::sync::Mutex;
 use once_cell::sync::Lazy;
 use crate::device::network::manager::{NetworkManager, NetworkManagerImpl};
-use super::X86HttpClientProvider;
 
 // Global network manager instance with platform-specific HTTP client provider
 static NETWORK_MANAGER: Lazy<Mutex<NetworkManagerImpl>> = Lazy::new(|| {
-    Mutex::new(NetworkManagerImpl::new(Box::new(X86HttpClientProvider::new())))
+    Mutex::new(NetworkManagerImpl::new())
 });
 
 /// Get the global network manager instance
